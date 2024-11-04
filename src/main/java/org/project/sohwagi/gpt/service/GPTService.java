106101c887749a7397aa28d2e14fb28353e80851
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-11-04 19:06:56 +0900

    refactor: serviceimpl 제거

--- src/main/java/org/project/sohwagi/gpt/service/GPTService.java
+++ src/main/java/org/project/sohwagi/gpt/service/GPTService.java
@@ -1,10 +1,63 @@
 package org.project.sohwagi.gpt.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.project.sohwagi.gpt.dto.GPTRequest;
+import org.project.sohwagi.gpt.dto.GPTResponse;
+import org.project.sohwagi.gpt.dto.Message;
 import org.project.sohwagi.schedule.dto.ScheduleRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-public interface GPTService {
+@Service
+@RequiredArgsConstructor
+public class GPTService {
 
-	ScheduleRequest gptCall(String prompt) throws JsonProcessingException;
+	@Value("${openai.model:gpt-4o}")
+	private String model;
+
+	@Value("${openai.api.url:https://api.openai.com/v1/chat/completions}")
+	private String apiUrl;
+
+	private final RestTemplate restTemplate;
+
+	private final ObjectMapper objectMapper;
+
+	public ScheduleRequest gptCall(String prompt) throws JsonProcessingException {
+		String SYSTEM_MESSAGE =
+			"너는 한 문장에서 일정 관련 정보를 추출하는 역할이야. "
+				+ "prompt 문장을 일정으로 등록하려는데 JSON 형태로 일정 제목, 일정 날짜로 분류해줘. 해당 값이 없으면 null 표시해줘."
+				+ "아래 양식 꼭 지켜줘. 현재 날짜는 "
+				+ LocalDateTime.now()
+				+ "이야. title, date 는 String 타입이고 date 예시: 10월 7일 (월)";
+
+		List<Message> messages = new ArrayList<>();
+		messages.add(new Message("system", SYSTEM_MESSAGE));
+		messages.add(new Message("user", prompt));
+
+		GPTRequest request = new GPTRequest(
+			model, 0, 256, 1, 0, 0, messages);
+
+		GPTResponse gptResponse = restTemplate.postForObject(
+			apiUrl
+			, request
+			, GPTResponse.class
+		);
+
+		String rawJsonString = gptResponse.getChoices().get(0).getMessage().getContent();
+
+		String jsonString = rawJsonString
+			.replace("```json", "")
+			.replace("```", "")
+			.trim();
+
+		return objectMapper.readValue(jsonString,
+			ScheduleRequest.class);
+	}
 
 }
--- src/main/java/org/project/sohwagi/user/service/UserService.java
+++ src/main/java/org/project/sohwagi/user/service/UserService.java
@@ -1,11 +1,29 @@
 package org.project.sohwagi.user.service;
 
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.project.sohwagi.user.entity.User;
+import org.project.sohwagi.user.repository.UserJpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserService {
+@Service
+@RequiredArgsConstructor
+public class UserService {
 
-	Optional<User> findUserByFcmToken(String fcmToken);
+	private final UserJpaRepository userJpaRepository;
 
-	void login(String fcmToken);
+	public Optional<User> findUserByFcmToken(String fcmToken) {
+		return userJpaRepository.findByFcmToken(fcmToken);
+	}
+
+	@Transactional
+	public void login(String fcmToken) {
+		Optional<User> savedUser = findUserByFcmToken(fcmToken);
+
+		if(savedUser.isEmpty()){
+			User user = new User(fcmToken);
+			userJpaRepository.save(user);
+		}
+	}
 }
