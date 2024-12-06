87fb7914031ce1b1d90b3963f3ec3d75d5b5415e
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-12-06 15:08:57 +0900

    CORS 설정 - OPTIONS 메서드 허용

--- src/main/java/org/project/sohwagi/util/config/WebConfig.java
+++ src/main/java/org/project/sohwagi/util/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOriginPatterns("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("x-nickname", "Content-Type", "Authorization")
         .allowCredentials(true);
   }
