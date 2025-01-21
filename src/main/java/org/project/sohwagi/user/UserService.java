d15ff7a783f3655e8633f489703fe85a492f3675
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-01-21 22:26:32 +0900

    refactor : 더보기 기능

--- src/main/java/org/project/sohwagi/user/UserService.java
+++ src/main/java/org/project/sohwagi/user/UserService.java
@@ -2,6 +2,7 @@ package org.project.sohwagi.user;
 
 import lombok.RequiredArgsConstructor;
 import org.project.sohwagi.common.UseCase;
+import org.project.sohwagi.user.dto.res.GetUserInfoRes;
 import org.project.sohwagi.user.dto.service.SaveFcmTokenCommand;
 import org.project.sohwagi.user.dto.service.GetOrCreateUserCommand;
 import org.springframework.stereotype.Service;
@@ -32,10 +33,30 @@ public class UserService {
     return user.getId();
   }
 
-  public UserDetails loadUserById(Long id){
+  public UserDetails loadUserById(Long id) {
 
     User user = userRepository.findById(id);
 
     return UserDetails.from(user);
   }
+
+  public GetUserInfoRes getUserInfo(UserDetails userDetails){
+    String name = convertName(userDetails.userName());
+
+    return new GetUserInfoRes(name, userDetails.email());
+  }
+
+  private String convertName(String name) {
+    if (name.contains(" ")) {
+      String[] parts = name.split(" ");
+      if (parts.length == 2) {
+        String firstName = parts[0];
+        String lastName = parts[1];
+
+        return lastName + firstName;
+      }
+    }
+    return name;
+  }
+
 }
