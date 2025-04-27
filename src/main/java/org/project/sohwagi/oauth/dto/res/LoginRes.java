a55a4c1192b8df2e9f4ff0fe6d3fb30d9aadf834
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-04-28 03:23:30 +0900

    #63 feat : QA 로그인 API 구현

--- src/main/java/org/project/sohwagi/oauth/dto/res/LoginRes.java
+++ src/main/java/org/project/sohwagi/oauth/dto/res/LoginRes.java
@@ -5,13 +5,13 @@ import static org.project.sohwagi.common.validation.Validation.validate;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
-public record AppleLoginRes(
+public record LoginRes(
     @NotNull String accessToken,
 
     @NotNull String refreshToken
 ){
   @Builder
-  public AppleLoginRes(String accessToken, String refreshToken) {
+  public LoginRes(String accessToken, String refreshToken) {
     this.accessToken = accessToken;
     this.refreshToken = refreshToken;
     validate(this);
