8d4f65a7d0fe8b2c5f66d0079e28e54fabf469d3
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-05-19 22:31:00 +0900

    hotfix : 프롬프트 수정 - dev, prod 동기화

--- src/main/java/org/project/sohwagi/schedule/adapter/out/gpt/GptAdapter.java
+++ src/main/java/org/project/sohwagi/schedule/adapter/out/gpt/GptAdapter.java
@@ -32,12 +32,12 @@ public class GptAdapter implements CallGptPort {
 	@Override
 	public ScheduleRequest callGptForTextSchedule(String prompt) throws JsonProcessingException {
 		String SYSTEM_MESSAGE =
-			"너는 한 문장에서 일정 관련 정보를 추출하는 역할이야. "
-				+ "prompt 문장을 일정으로 등록하려는데 JSON 형태로 일정 제목, 일정 날짜로 분류해줘. 해당 값이 없으면 null 표시해줘."
-					+ "일정 날짜는 꼭 월, 일, 요일 나타내야하는데 값이 없으면 계산해서 알려줘."
-				+ "아래 양식 꼭 지켜줘. 현재 날짜는 "
-				+ LocalDateTime.now()
-				+ "이야. title, date 는 String 타입이고 date 예시: 10월 7일 월요일";
+				"너는 한 문장에서 일정 관련 정보를 추출하는 역할이야. "
+						+ "prompt 문장을 일정으로 등록하려는데 JSON 형태로 일정 제목, 일정 날짜로 분류해줘. 해당 값이 없으면 null 표시해줘."
+						+ "일정 날짜는 꼭 월, 일, 요일, 시각 나타내야하는데 값이 없으면 계산해서 알려줘."
+						+ "아래 양식 꼭 지켜줘. 현재 날짜는 "
+						+ LocalDateTime.now()
+						+ "이야. title, date 는 String 타입이고 date 예시: 2025년 4월 7일 월요일 오전 9시 00분";
 
 		List<Message> messages = new ArrayList<>();
 		messages.add(new Message("system", SYSTEM_MESSAGE));
