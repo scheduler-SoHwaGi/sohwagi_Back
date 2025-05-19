296c7237d9b3d8295820d995f79f7b81fb646e8b
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-05-19 23:55:00 +0900

    #69 feat : FirebaseInitialization 추가

---
+++ src/main/java/org/project/sohwagi/config/FirebaseInitialization.java
@@ -0,0 +1,30 @@
+package org.project.sohwagi.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirebaseInitialization {
+
+  @PostConstruct
+  public void init() {
+    try {
+      FileInputStream serviceAccount = new FileInputStream(
+          "src/main/resources/firebase-service-account.json");
+
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .build();
+
+      FirebaseApp.initializeApp(options);
+    }catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
