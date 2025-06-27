d8a46798188c3b4448c8a6ef433128c9834abec2
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-06-28 01:53:00 +0900

    #81 feat : StatusGenerator 추가 - 일정 수에 따라 Status 반환

--- src/main/java/org/project/sohwagi/application/facade/ScheduleFacadeService.java
+++ src/main/java/org/project/sohwagi/application/facade/ScheduleFacadeService.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.project.sohwagi.application.StatusGenerator;
 import org.project.sohwagi.application.cmd.ScheduleCommand.ScheduleCheckCommand;
 import org.project.sohwagi.application.cmd.ScheduleCommand.ScheduleCountCommand;
 import org.project.sohwagi.application.cmd.ScheduleCommand.ScheduleCreateByTextCommand;
@@ -15,6 +16,7 @@ import org.project.sohwagi.application.info.ScheduleInfo.ScheduleCountsInfo;
 import org.project.sohwagi.application.info.ScheduleInfo.ScheduleDetailsInfo;
 import org.project.sohwagi.application.service.ScheduleService;
 import org.project.sohwagi.application.info.ScheduleInfo.ScheduleDetailInfo;
+import org.project.sohwagi.domain.Schedule;
 import org.project.sohwagi.infra.llm.LlmClient;
 import org.project.sohwagi.infra.llm.LlmResult.ExtractedScheduleInformation;
 import org.springframework.stereotype.Service;
@@ -27,12 +29,13 @@ public class ScheduleFacadeService {
   private final ScheduleService scheduleService;
 
   public ScheduleCountsInfo getScheduleCounts(ScheduleCountCommand cmd) {
-    Map<String, Integer> result = scheduleService.getScheduleCounts(cmd);
+    Map<String, List<Schedule>> result = scheduleService.getScheduleCounts(cmd);
 
     List<ScheduleCountInfo> infos = result.entrySet().stream()
         .map(entry -> new ScheduleCountInfo(
             entry.getKey(),
-            entry.getValue())
+            entry.getValue().size(),
+            StatusGenerator.generateStatus(entry.getValue().size(), entry.getValue()))
         ).toList();
 
     return new ScheduleCountsInfo(infos);
