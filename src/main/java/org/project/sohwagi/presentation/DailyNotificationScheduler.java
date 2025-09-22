c3617e7b7a06397f9cab6a8b8dd47da4550ee909
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-09-23 00:17:37 +0900

    #103 feat : FCM 푸시 알림 전송 비동기 스레드 풀 지정

--- src/main/java/org/project/sohwagi/presentation/DailyNotificationScheduler.java
+++ src/main/java/org/project/sohwagi/presentation/DailyNotificationScheduler.java
@@ -14,7 +14,7 @@ public class DailyNotificationScheduler {
     this.scheduleNotificationService = scheduleNotificationService;
   }
 
-  @Scheduled(cron = "0 31 22 * * *", zone = "Asia/Seoul")
+  @Scheduled(cron = "0 00 09 * * *", zone = "Asia/Seoul")
   public void notifyTodaySchedules() throws FirebaseMessagingException {
     scheduleNotificationService.sendDailyScheduleNotifications();
   }
