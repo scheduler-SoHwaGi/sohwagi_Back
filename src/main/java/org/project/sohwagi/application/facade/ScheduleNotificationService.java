7ed195a8aa7c88f10c4197aa4f14beb0dbe223fb
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-12-16 00:34:12 +0900

    #143 refactor : 푸시 알림 title 변경

--- src/main/java/org/project/sohwagi/application/facade/ScheduleNotificationService.java
+++ src/main/java/org/project/sohwagi/application/facade/ScheduleNotificationService.java
@@ -96,7 +96,7 @@ public class ScheduleNotificationService {
       if(user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
         continue;
       }
-      String name = user.getFcmToken().split(" ")[0];
+      String name = user.getUserName().split(" ")[0];
       String title = name + "햄! 잠깐만, 이 일정 잊지 않았지?";
       String body = "15분 후에 일정이 있어! 미리 준비하면 딱 좋아햄!";
       firebaseMessagingClient.sendMessage(user.getFcmToken(), title, body);
