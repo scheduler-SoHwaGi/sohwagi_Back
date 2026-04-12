6ed16f46c3342574ea1f35e026a5abd929eccad3
OZIIJIN <dpwlsdlqma6@gmail.com>
2026-04-12 22:22:04 +0900

    #143 refactor : 스케줄 데일리 노티 스케줄 타입 필터링 추가

--- src/main/java/org/project/sohwagi/application/facade/ScheduleNotificationService.java
+++ src/main/java/org/project/sohwagi/application/facade/ScheduleNotificationService.java
@@ -1,8 +1,6 @@
 package org.project.sohwagi.application.facade;
 
-import com.google.firebase.messaging.FirebaseMessagingException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -11,9 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.project.sohwagi.application.service.ScheduleService;
 import org.project.sohwagi.application.service.UserService;
 import org.project.sohwagi.domain.Schedule;
+import org.project.sohwagi.domain.ScheduleType;
 import org.project.sohwagi.domain.User;
 import org.project.sohwagi.infra.firebase.FirebaseMessagingClient;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -25,7 +23,7 @@ public class ScheduleNotificationService {
   private final UserService userService;
 
   public ScheduleNotificationService(ScheduleService scheduleService,
-      FirebaseMessagingClient firebaseMessagingClient, UserService userService) {
+    FirebaseMessagingClient firebaseMessagingClient, UserService userService) {
     this.scheduleService = scheduleService;
     this.firebaseMessagingClient = firebaseMessagingClient;
     this.userService = userService;
@@ -34,31 +32,33 @@ public class ScheduleNotificationService {
   public void sendDailyScheduleNotifications() {
     LocalDate today = LocalDate.now();
 
-    List<Schedule> todaySchedules = scheduleService.findTodaySchedules(today);
+    List<Schedule> todaySchedules = scheduleService.findTodaySchedules(today).stream()
+      .filter(s -> s.getType().equals(
+        ScheduleType.SCHEDULE)).toList();
     if (todaySchedules.isEmpty()) {
       return;
     }
 
     Map<Long, List<Schedule>> scheduleMap = todaySchedules.stream()
-        .collect(Collectors.groupingBy(Schedule::getUserId));
+      .collect(Collectors.groupingBy(Schedule::getUserId));
     Set<Long> userIds = scheduleMap.keySet();
     Map<Long, User> userMap = userService.findAllUserByIdIn(userIds).stream()
-        .collect(Collectors.toMap(User::getId, user -> user));
+      .collect(Collectors.toMap(User::getId, user -> user));
 
     for (Map.Entry<Long, List<Schedule>> entry : scheduleMap.entrySet()) {
       Long userId = entry.getKey();
       User user = userMap.get(userId);
-      if(user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
+      if (user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
         continue;
       }
       List<Schedule> schedules = entry.getValue();
       String title = String.format("오늘 일정 %d개다 햄+_+", schedules.size());
       String body = String.format(
-          "오늘 %s %02d:%02d에 %s이(가) 있어요!",
-          schedules.get(0).getAmPm(),
-          schedules.get(0).getHour(),
-          schedules.get(0).getMinute(),
-          schedules.get(0).getTitle());
+        "오늘 %s %02d:%02d에 %s이(가) 있어요!",
+        schedules.get(0).getAmPm(),
+        schedules.get(0).getHour(),
+        schedules.get(0).getMinute(),
+        schedules.get(0).getTitle());
 
       firebaseMessagingClient.sendMessage(user.getFcmToken(), title, body);
     }
@@ -79,7 +79,7 @@ public class ScheduleNotificationService {
   public void sendDailyScheduleRegistrationNotifications() {
     List<User> users = userService.findDistinctUsersByFcmToken();
     for (User user : users) {
-      if(user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
+      if (user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
         continue;
       }
       String name = user.getUserName().split(" ")[0];
@@ -93,7 +93,7 @@ public class ScheduleNotificationService {
     List<Schedule> schedules = scheduleService.findSchedulesToNotify();
     for (Schedule schedule : schedules) {
       User user = userService.findById(schedule.getUserId());
-      if(user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
+      if (user.getFcmToken() == null || user.getFcmToken().isEmpty()) {
         continue;
       }
       String name = user.getUserName().split(" ")[0];
